1502b5943f53caab6a37ad136fa6c83d7ac90c6b
Hassani <testhassanhamza@gmail.com>
2025-11-26 17:48:44 +0300

    login + simple protection

--- public/protect.js
+++ public/protect.js
@@ -1,6 +1,8 @@
-// protect.js
+// public/protect.js
+
 const token = localStorage.getItem("token");
 
+// Si pas de token = pas connecté → on renvoie vers le login
 if (!token) {
-    window.location.href = "/login.html";
+  window.location.href = "/login.html";
 }
--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,17 @@
+// routes/auth.js
 import express from "express";
 const router = express.Router();
 
-const ADMIN_PASSWORD = "12345WivoZone"; // Tu peux changer
+const ADMIN_PASSWORD = "12345WivoZone"; // tu peux changer ici
 
 router.post("/login", (req, res) => {
   const { password } = req.body;
 
   if (password === ADMIN_PASSWORD) {
-    // token simple
     const token = Buffer.from(Date.now().toString()).toString("base64");
-
     return res.json({
       success: true,
-      token
+      token,
     });
   }
 
